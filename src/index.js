4714c0a23c3947f88011db6e28326e9df04e0e0d
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-18 19:12:13 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+
 let addToy = false
 
 document.addEventListener("DOMContentLoaded", ()=>{
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container')
+
   addBtn.addEventListener('click', () => {
     // hide & seek with the form
     addToy = !addToy
@@ -12,5 +14,62 @@ document.addEventListener("DOMContentLoaded", ()=>{
       toyForm.style.display = 'none'
     }
   })
-
+  getToys()
 })
+
+function getToys() {
+  return fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(obj => addToyCard(obj))
+}
+
+function addToyCard(obj) {
+  const collection = document.getElementById('toy-collection')
+  obj.forEach(function(toy) {
+    const div = document.createElement('div')
+    div.className = 'card'
+
+    const h2 = document.createElement('h2')
+    h2.innerText = toy.name
+
+    const img = document.createElement('img')
+    img.src = toy.image
+    img.className = 'toy-avatar'
+
+    const p = document.createElement('p')
+    p.innerText = `${toy.likes} Likes`
+
+    const btn =  document.createElement('button')
+    btn.className = 'like-btn'
+    btn.innerText = 'Like <3'
+    btn.id = toy.id
+    btn.addEventListener('click', (e) => {
+    console.log(e.target.dataset);
+    likes(e)
+    })
+
+    div.append(h2, img, p, btn)
+    collection.appendChild(div)
+  })
+
+  function likes(e) {
+  e.preventDefault()
+  let more = parseInt(e.target.previousElementSibling.innerText) + 1
+
+  fetch(`http://localhost:3000/toys/${e.target.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+
+      },
+      body: JSON.stringify({
+        "likes": more
+      })
+    })
+    .then(res => res.json())
+    .then((like_obj => {
+      e.target.previousElementSibling.innerText = `${more} likes`;
+    }))
+}
+}
