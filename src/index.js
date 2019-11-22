34471f7199483214233c4d0d5cb54b6d4d4c4b73
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-21 16:08:29 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -14,3 +14,38 @@ document.addEventListener("DOMContentLoaded", ()=>{
   })
 
 })
+
+function getToy() {
+  // 2
+  fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  // 3
+  .then(toys => {
+    // iterate through toys 
+    toys.forEach(toy => displayToy(toy))
+  })
+}
+
+// html template card
+// 5
+function toyCard(toy){
+  return `
+  <div class="card">
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p> ${toy.likes} </p>
+    <button class="like-btn">Like <3</button>
+  </div>
+  `
+}
+
+// attach to DOM 
+// 4
+const displayToy = toy => {
+  const toyCollection = document.querySelector("#toy-collection");
+  // debugger
+  toyCollection.innerHTML += toyCard(toy)
+}
+
+// 1
+getToy()
